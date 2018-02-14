5f170da8583f62f85e8226c8ceac4c546d25be14
jiangshanmeta <540118044@qq.com>
2018-02-14 10:39:16 +0800

    基本实现

--- example/app.vue
+++ example/app.vue
@@ -1,11 +1,97 @@
 <template>
     <section>
-        112
+        <table>
+            <tr>
+                <th>id</th>
+                <td>{{id}}</td>
+            </tr>
+            <tr>
+                <th>userName</th>
+                <td>{{userName}}</td>
+            </tr>
+            <tr>
+                <th>userPhone</th>
+                <td>{{userPhone}}</td>
+            </tr>
+            <tr>
+                <th>userMail</th>
+                <td>{{userMail}}</td>
+            </tr>
+            <tr>
+                <th>userInfo</th>
+                <td>{{userInfo}}</td>
+            </tr>
+        </table>
+
+        <button @click="increaseId">increase id</button>
     </section>
 </template>
 
 <script>
 export default{
+    data(){
+        return {
+            id:null,
+            counter:0,
+        };
+    },
+    computed:{
+        userInfo:{
+            get(){
+                return `${this.userName} || ${this.userMail}`
+            },
+            cache:false,
+        }
+    },
+    asyncComputed:{
+        userName(){
+            return new Promise((resolve)=>{
+                let id = this.id;
 
+                setTimeout(()=>{
+                    resolve(`name for ${id}`);
+                },2000);
+            });
+        },
+        userPhone:{
+            get(){
+                return new Promise((resolve)=>{
+                    let id = this.id;
+                    setTimeout(()=>{
+                        resolve(`phone for ${id}`);
+                    },2000)
+                })
+            },
+            default:"loading phone ~~~"
+        },
+        userMail:{
+            get(){
+                return new Promise((resolve)=>{
+                    let id = this.id;
+                    setTimeout(()=>{
+                        resolve(`mail for ${id} and counter is ${this.counter}`)
+                    },2000);
+                })
+            },
+            watch(){
+                return this.counter;
+            },
+            default(){
+                return `loading mail for ${this.id} with counter is ${this.counter}`
+            },
+        }
+
+    },
+    methods:{
+        increaseId(){
+            this.id++;
+        },
+    },
+    created(){
+        this.id = 1;
+        setInterval(()=>{
+            this.counter++;
+        },5000)
+    }
 }
 </script>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,71 @@
+function _genDefault(defaultVal,vm){
+    return typeof defaultVal === 'function'?defaultVal.call(vm):defaultVal;
+}
+
+function defineAsyncComputed(vm,key,userDef){
+    let getter;
+    let defaultVal = null;
+    let lazy = false;
+    let watch;
+    
+    if(typeof userDef === 'function'){
+        getter = userDef;
+    }else{
+        getter = userDef.get;
+        if(userDef.hasOwnProperty('default')){
+            defaultVal = userDef.default;
+        }
+        if(typeof userDef.watch === 'function'){
+            watch = userDef.watch;
+        }
+    }
+
+    let val = _genDefault(defaultVal,vm);
+
+    Object.defineProperty(vm,key,{
+        get(){
+            return val;
+        },
+    });
+
+    vm.$watch(function(){
+        watch && watch.call(this);
+        val = _genDefault(defaultVal,vm);
+        vm.$forceUpdate();
+        return getter.call(this);
+    },function(promise){
+
+        if(!(promise instanceof Promise)){
+            promise = Promise.resolve(promise);
+        }
+
+        promise.then((newVal)=>{
+            val = newVal;
+            vm.$forceUpdate();
+        });
+
+
+    },{
+        lazy
+    })
+
+}
+
+
 export default {
-    install(){
-        
+    install(Vue){
+        Vue.mixin({
+            created(){
+                const asyncComputed = this.$options.asyncComputed;
+                if(!asyncComputed || typeof asyncComputed !== 'object'){
+                    return;
+                }
+
+                Object.keys(asyncComputed).forEach((key)=>{
+                    defineAsyncComputed(this,key,asyncComputed[key])
+                })
+
+            }
+        })
     }
 }
